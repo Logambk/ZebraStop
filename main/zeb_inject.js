d6f53ba507c047b7699a21417e0024ca54688908
Logam <logambk@gmail.com>
2016-08-08 19:15:58 -0500

    added support for pop-up messages,
    added support for mobile version (m.vk and 0.vk)

--- main/zeb_inject.js
+++ main/zeb_inject.js
@@ -10,19 +10,24 @@
 		}
 	});
 	
-	function updateComments(id) {
-		var initialPosts = document.getElementById(id);
-		if (initialPosts) {
-			var comments = initialPosts.querySelectorAll('a.author[data-from-id="4170620"]');
-			for (var i = 0; i < comments.length; i++) {
-				updateComment(comments[i]);
-			}
+	function updateComments(selector) {
+		var initialRoot = document.querySelector(selector);
+		if (initialRoot) {
+			updateSubSelector(initialRoot, 'a.author[data-from-id="4170620"]');
+			updateSubSelector(initialRoot, '.al_u4170620');
+		}
+	}
+	
+	function updateSubSelector(root, selector) {
+		var comments = root.querySelectorAll(selector);
+		for (var i = 0; i < comments.length; i++) {
+			updateComment(comments[i]);
 		}
 	}
 
 	function updateComment(row) {
 		var par = row.parentNode;
-		while (!par.classList.contains('reply')) {
+		while (!par.classList.contains('reply') && !par.classList.contains('comment_item')) {
 			par = par.parentNode;
 		}
 		
@@ -34,7 +39,9 @@
 	}
 	
 	setInterval(function() {
-		updateComments('page_wall_posts');
-		updateComments('feed_wall');
+		updateComments('#page_wall_posts');	//group wall
+		updateComments('#feed_wall');		//feed page
+		updateComments('#wk_box');			//pop-up post
+		updateComments('.wall_replies');	//pop-up for moblie version		
 	}, 500);
 })();
